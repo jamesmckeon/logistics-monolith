feb35a950aaf16aad48503fdfbba48b9a6cdb3f0
Jim McKeon <jbmckeon@comcast.net>
2026-08-20 10:12:38 -0700

    feat: added api projects

---
+++ src/modules/ordering/Throughline.Modules.Ordering.Api/OrderingApi.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Throughline.Modules.Ordering.Application.CreateOrder;
+using Throughline.Modules.Ordering.Application.Orders.Models;
+
+namespace Throughline.Modules.Ordering.Api;
+
+public static class OrderingExtensions
+{
+    public static IServiceCollection AddOrdering(this IServiceCollection services, IConfiguration config)
+    {
+        return services;
+    }
+
+    public static IEndpointRouteBuilder MapOrdering(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/orders").WithTags("Ordering");
+        group.MapPost("/", async (CancellationToken token,
+            CreateOrderRequest request, ICreateOrderHandler handler) =>
+        {
+            var commandResult = CreateOrderCommand.Create(
+                request.MerchantId,
+                request.PurchaseOrderNumber,
+                request.StreetAddressOne,
+                request.StreetAddressTwo,
+                request.City,
+                request.State,
+                request.PostalCode,
+                request.Items,
+                request.ReferenceNumber);
+
+            if (!commandResult.Succeeded)
+                return Results.BadRequest();
+
+            var result = await handler.CreateOrderAsync(commandResult.Value, token);
+
+            if (result.Succeeded)
+                return result.Value;
+            // what do i return here
+            throw new NotImplementedException();
+        });
+
+        return app;
+    }
+}
