d5fbc778420b174db3b7e3c7a714cec059fd4b9f
jamesmckeon <jbmckeon@comcast.net>
2026-08-30 07:20:54 -0400

    refactor: finished moving items from billing to ordering module

---
+++ Throughline.Modules.Ordering/Presentation/OrderingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Throughline.Common.Presentation;
+using Throughline.Modules.Ordering.Application;
+using Throughline.Modules.Ordering.Domain.Orders;
+using Throughline.Modules.Ordering.Infrastructure.Orders;
+
+namespace Throughline.Modules.Ordering.Presentation;
+
+public static class OrderingExtensions
+{
+    public static IServiceCollection AddOrdering(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddTransient<ICreateOrderHandler, CreateOrderHandler>();
+        services.AddTransient<IOrdersRepository, OrdersRepository>();
+        return services;
+    }
+
+    public static IEndpointRouteBuilder MapOrdering(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/orders").WithTags("Ordering");
+
+        group.MapPost("/", async (CancellationToken token,
+            CreateOrderCommand command, ICreateOrderHandler handler) =>
+        {
+            var result = await handler.CreateOrderAsync(command, token);
+            return result.ToTypedResult();
+        });
+
+        return app;
+    }
+}
