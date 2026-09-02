a1c6b9536dadd22327f4038192523f8972c2253e
jamesmckeon <jbmckeon@comcast.net>
2026-09-02 15:19:59 -0400

    feat: added migration

---
+++ src/modules/ordering/Throughline.Modules.Ordering/Infrastructure/Migrations/20260902191854_Initial.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Throughline.Modules.Ordering.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "orders");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MerchantId = table.Column<int>(type: "integer", nullable: false),
+                    PurchaseOrderNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ReferenceNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    StreetAddressOne = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    StreetAddressTwo = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true),
+                    City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    State = table.Column<string>(type: "character(2)", fixedLength: true, maxLength: 2, nullable: false),
+                    Zipcode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLines",
+                schema: "orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SkuCode = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "orders",
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderId",
+                schema: "orders",
+                table: "OrderLines",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_MerchantId_ReferenceNumber",
+                schema: "orders",
+                table: "Orders",
+                columns: new[] { "MerchantId", "ReferenceNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderLines",
+                schema: "orders");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "orders");
+        }
+    }
+}
